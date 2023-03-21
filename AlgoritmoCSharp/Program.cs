894cf11bb42bb8c3e684e83189c54c819d34a2ed
Ellen <36012338+engineerellen@users.noreply.github.com>
2023-03-20 22:28:53 -0300

    Add project files.

---
+++ AlgoritmoCSharp/Program.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace AlgoritmoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConverterJson();
+            Console.ReadLine();
+        }
+
+        public static void ConverterJson()
+        {
+            WebClient client = new WebClient();
+            string s = client.DownloadString("https://coderbyte.com/api/challenges/json/age-counting");
+
+            string jsonString = s.Replace("\r\n", "").Replace("\n", "").Replace("\r", "").Replace(" ", "");
+            var jsonContent = jsonString.Replace(@"{""data"":""", "").Replace("}", "");
+            var contentArr = jsonContent.Split(',');
+
+            var countAge = 0;
+
+            foreach ( var item in contentArr ) 
+            {
+                int age = int.TryParse(item.Split('=').Last(), out var parseAge) ? parseAge : 0;
+
+                if (age >= 50) 
+                    countAge++;
+            }
+            Console.WriteLine($"There are {countAge} people greater 50 years old!");
+            Console.ReadLine();
+
+        }
+
+        #region Diferenca
+
+        /// <summary>
+        /// Implemente o método que calcule a diferença que o usuário deverá pagar ao fazer 
+        //upgrade do plano de assinatura.Com o valor retornado pelo méotodo será feita a
+        //cobrança extra da diferença entre os planos no ato do upgrade referente aos dias
+        //restantes, sem alterar a data de pagamento do cliente.
+        //Por exemplo: Usuário assinou o plano R$ 30,00 no dia 01/01, no dia 09/01 ele fez
+        //upgrade para o plano de R$ 60,00, então ele precisa pagar a diferença relativa dos
+        //dias restante.
+        /// </summary>
+        /// 
+        public static double ValorDiferenca(double valorPlanoInicial, double valorPlanoUpdate, DateTime dataInicial, DateTime dataUpgrade)
+        {
+            double valorDiarioInicial = valorPlanoInicial / 30;
+            double valorDiarioUpgrade = valorPlanoUpdate / 30;
+
+
+            if (dataUpgrade < dataInicial)
+                return 0;
+
+
+            int diferencaDias = 30 - (dataUpgrade.Subtract(dataInicial)).Days;
+
+            double valorMensalPlanoUpgrade = valorDiarioUpgrade * (31 - dataUpgrade.Day);
+            double valorMensalPlanoInicial = valorDiarioInicial * (31 - diferencaDias);
+
+            return valorMensalPlanoUpgrade + valorMensalPlanoInicial;
+
+
+
+
+        }
+        #endregion
+
+        #region Algoritmo simples
+
+        /*
+         * Fazer um programa para ler o código de uma peça 1, o número de peças 1,
+         * o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2
+         * e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.
+         */
+        static void Ex1()
+        {
+            Console.WriteLine("Digite o código da primeira peça:");
+            int codigoP1 = int.TryParse(Console.ReadLine(), out codigoP1) ? codigoP1 : 0;
+
+
+            Console.WriteLine("Digite o numero da primeira peça:");
+            int qtdeP1 = int.TryParse(Console.ReadLine(), out qtdeP1) ? qtdeP1 : 0;
+
+            Console.WriteLine("Digite o valor unitario da primeira peça:");
+            double valorP1 = double.TryParse(Console.ReadLine(), out valorP1) ? valorP1 : 0;
+
+            double valorPagarP1 = 0;
+
+            valorPagarP1 = qtdeP1 * valorP1;
+
+            Console.WriteLine("Digite o código da segunda peça:");
+            int codigoP2 = int.TryParse(Console.ReadLine(), out codigoP2) ? codigoP2 : 0;
+
+
+            Console.WriteLine("Digite o numero da segunda peça:");
+            int qtdeP2 = int.TryParse(Console.ReadLine(), out qtdeP2) ? qtdeP2 : 0;
+
+            Console.WriteLine("Digite o valor unitario da segunda peça:");
+            double valorP2 = double.TryParse(Console.ReadLine(), out valorP2) ? valorP2 : 0;
+
+            double valorPagarP2 = 0;
+
+            valorPagarP2 = qtdeP2 * valorP2;
+
+            double valorTotal = valorPagarP1 + valorPagarP2;
+
+            Console.WriteLine("Valor a pagar: " + valorTotal);
+
+        }
+
+        /*
+         * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor 
+         * da área deste círculo com quatro casas decimais conforme exemplos.
+         */
+        static void Ex2()
+        {
+            Console.WriteLine("Digite o valor da raio:");
+            double raio = double.TryParse(Console.ReadLine(), out raio) ? raio : 0;
+
+            double pi = Math.PI;
+
+            double areaCirculo = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A área do ciruclo é : " + areaCirculo);
+        }
+
+        #endregion
+
+
+        #region Condições
+
+        /*
+         um programa que leia o código de um item e a quantidade deste item.A seguir, calcule e
+         mostre o valor da conta a pagar.
+        */
+        static void Ex21()
+        {
+            Console.WriteLine("Digite o código do produto: ");
+            int codigoP1 = int.TryParse(Console.ReadLine(), out codigoP1) ? codigoP1 : 0;
+
+            Console.WriteLine("Digite a quantidade do produto: ");
+            int qtdeP1 = int.TryParse(Console.ReadLine(), out qtdeP1) ? qtdeP1 : 0;
+
+            double valorTotal = 0;
+
+            Produto prd = new Produto();
+
+            List<Produto> lstPRoduto = prd.retornarLista();
+
+            foreach (var item in lstPRoduto)
+            {
+                if (item.Codigo == codigoP1)
+                {
+                    valorTotal = item.Preco * qtdeP1;
+
+                    Console.WriteLine("Total: " + valorTotal);
+                    return;
+                }
+            }
+            Console.WriteLine("Código do produto invalido!");
+        }
+
+        static void Ex22()
+        {
+            Console.WriteLine("Digite o valor de a: ");
+            double a = double.TryParse(Console.ReadLine(), out a) ? a : 0;
+
+            Console.WriteLine("Digite o valor de b: ");
+            double b = double.TryParse(Console.ReadLine(), out b) ? b : 0;
+
+            Console.WriteLine("Digite o valor de c: ");
+            double c = double.TryParse(Console.ReadLine(), out c) ? c : 0;
+
+            double delta = Math.Pow(b, 2) - 4 * a * c;
+
+
+            if (a == 0 || delta < 0)
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+                Console.WriteLine("x1: " + x1);
+                Console.WriteLine("x2: " + x2);
+            }
+
+        }
+        #endregion
+
+        #region Loop
+
+
+        /*
+         * Escreva um programa que repita a leitura de uma senha até que ela seja válida. 
+         * Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida".
+         * Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" 
+         * e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+         */
+        static void Ex31()
+        {
+            string senha = ObterSenha();
+            while (senha != "2002")
+            {
+                senha = ObterSenha();
+
+                if (senha == "2002")
+                {
+                    Console.WriteLine("Acesso Permitido!");
+                    break;
+                }
+            }
+
+
+        }
+
+        static string ObterSenha()
+        {
+            Console.WriteLine("Favor inserir a senha !");
+            return Console.ReadLine();
+        }
+
+        /*
+         * Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+           Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo,
+            mostrando essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, 
+            e "out" para fora do intervalo).
+         */
+        static void Ex32()
+        {
+            Console.WriteLine("Digite o valor de N (quantide de repetições): ");
+            int n = int.TryParse(Console.ReadLine(), out n) ? n : 0;
+
+            int valor = 0;
+
+            int[] matriz = new int[n];
+            int varIN = 0;
+            int varOUT = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite um número: ");
+                valor = int.TryParse(Console.ReadLine(), out valor) ? valor : 0;
+
+                matriz[i] = valor;
+            }
+
+            Console.WriteLine("\n");
+
+            for (int i = 0; i < matriz.Length; i++)
+            {
+                Console.WriteLine(matriz[i]);
+
+                if (matriz[i] >= 10 && matriz[i] <= 20)
+                {
+                    varIN++;
+                }
+                else
+                {
+                    varOUT++;
+                }
+            }
+            Console.WriteLine(varIN + " in ");
+            Console.WriteLine(varOUT + " out ");
+
+        }
+
+        #endregion
+    }
+}
+
+public class Produto
+{
+    public int Codigo { get; set; }
+    public string Especificacao { get; set; }
+    public double Preco { get; set; }
+
+    public Produto()
+    {
+
+    }
+    public Produto(int _codigo, string _especificacao, double _preco)
+    {
+        Codigo = _codigo;
+        Especificacao = _especificacao;
+        Preco = _preco;
+    }
+
+    public List<Produto> retornarLista()
+    {
+        List<Produto> lstProdutos = new List<Produto>();
+
+        Produto prd = new Produto(1, "Cachorro Quente", 4.00);
+        lstProdutos.Add(prd);
+
+        prd = new Produto(2, "X salada", 4.50);
+        lstProdutos.Add(prd);
+
+        prd = new Produto(3, "X bacon", 5.00);
+        lstProdutos.Add(prd);
+
+        prd = new Produto(4, "Torrada Simples", 2.00);
+        lstProdutos.Add(prd);
+
+        prd = new Produto(5, "Refrigerante", 1.50);
+        lstProdutos.Add(prd);
+
+
+        return lstProdutos;
+
+    }
+
+
+}
---
+++ Exercicios/Exercicios/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static int[] Saque(int[][] cedulasCaixa, int input)
+        {
+        inicio:
+
+            Dictionary<int, int> cedulas = new Dictionary<int, int>();
+
+            for(int i = 0; i<= 5; i++)
+            {
+                cedulas.Add(cedulasCaixa[i][0], cedulasCaixa[i][1]);
+            }           
+
+            Console.WriteLine("Teste Notas");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Digite o valor do saque: ");
+
+            var valorEntrada = input;
+
+            int valor = 0;
+
+            int.TryParse(valorEntrada.ToString(), out valor);
+
+
+
+
+            var validacaoNumerosInvalidos = valorEntrada.ToString()[(valorEntrada.ToString().Length - 1)].ToString();
+
+            if (validacaoNumerosInvalidos.Contains("3") ||
+                validacaoNumerosInvalidos.Contains("1") ||
+                valor == 0)
+            {
+
+                Console.WriteLine("Valor invalido para as notas disponiveis");
+                goto inicio;
+            }
+
+
+
+
+            int n100 = valor / 100;
+
+            if (cedulas.GetValueOrDefault(100) < n100)
+            {
+                Console.WriteLine("notas de 100 insuficientes");
+                goto inicio;
+
+            }
+
+            int resto = valor - (n100 * 100);
+
+            int n50 = resto / 50;
+
+            if (cedulas.GetValueOrDefault(50) < n50)
+            {
+                Console.WriteLine("notas de 50 insuficientes");
+                goto inicio;
+            }
+            resto = resto - (n50 * 50);
+
+            int n20 = resto / 20;
+            if (cedulas.GetValueOrDefault(20) < n20)
+            {
+                Console.WriteLine("notas de 20 insuficientes");
+                goto inicio;
+            }
+
+            resto = resto - (n20 * 20);
+
+            int n10 = resto / 10;
+            if (cedulas.GetValueOrDefault(10) < n10)
+            {
+                Console.WriteLine("notas de 10 insuficientes");
+                goto inicio;
+            }
+
+            resto = resto - (n10 * 10);
+
+            int n5 = resto / 5;
+            if (cedulas.GetValueOrDefault(5) < n5)
+            {
+                Console.WriteLine("notas de 5 insuficientes");
+                goto inicio;
+            }
+
+            resto = resto - (n5 * 5);
+
+            int n2 = resto / 2;
+
+            if (cedulas.GetValueOrDefault(2) < n2)
+            {
+                Console.WriteLine("notas de 2 insuficientes");
+                goto inicio;
+            }
+
+            resto = resto - (n2 * 2);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Quantidade de notas:");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 100R$:  " + n100);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 50R$:  " + n50);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 20R$:  " + n20);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 10R$:  " + n10);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 5R$:  " + n5);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Notas de 2R$:  " + n2);
+
+            Console.ReadKey();
+            goto inicio;
+            return null;
+        }
+
+        static void Exercicio1()
+        {
+            int[][] cedulas = new int[][]
+            {
+                new int[] { 2, 10 },
+                new int[] { 5, 1 },
+                new int[] { 10, 2 },
+                new int[] { 20, 3 },
+                new int[] { 50, 3 },
+                new int[] { 100, 2 }
+            };
+            var input = Convert.ToInt32(Console.ReadLine());
+            Saque(cedulas, input);
+          
+        }
+        static int Fib(int n)
+        {
+            if(n == 1)
+            {
+                return n;
+            }
+            if(n== 0)
+            {
+                return 0;
+            }
+
+            int fib = Fib(n - 1) + Fib(n - 2);
+            return fib;
+        }
+
+        static int FibEllen(int n)
+        {
+
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            for (int i = 0; i < n; i++)
+            {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return a;
+        }
+        static void Exercicio2()
+        {
+            inicio:
+            Console.WriteLine("Execução do algoritmo original");
+            Console.WriteLine("");
+            Console.WriteLine("Digite o tamanho da sequencia");
+            string input = Console.ReadLine();
+           // Console.ReadKey();
+            int valor = Convert.ToInt32(input);
+            var stop = new Stopwatch();
+            stop.Start();
+            var result = Fib(valor);
+            stop.Stop();
+            var time = stop.Elapsed;
+            Console.WriteLine($"Tempo da execução do algoritmo original: {result} {time}s");
+            Console.WriteLine("Execução do algoritmo da Ellen");
+            Console.WriteLine("");
+         
+            var stopEllen = new Stopwatch();
+            stopEllen.Start();
+            var resultEllen = 0;
+             resultEllen = FibEllen(valor);
+            stopEllen.Stop();
+             var timeEllen = stopEllen.Elapsed;
+            Console.WriteLine($"Tempo da execução do algoritmo da Ellen: {resultEllen} {timeEllen}s");
+            Console.ReadKey();
+            goto inicio;
+        }
+        static void Main(string[] args)
+        {
+
+            // Exercicio1();
+            Exercicio2();
+        }
+    }
+}
